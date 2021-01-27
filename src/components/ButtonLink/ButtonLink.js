ef5b69cdc7e494ec38f60d27a860020b31880a87
Raul Zarza <raulzarza.dev@gmail.com>
2021-01-27 14:25:13 -0600

    formated

--- src/components/ButtonLink/ButtonLink.js
+++ src/components/ButtonLink/ButtonLink.js
@@ -1,11 +1,11 @@
-import Link from "next/link";
-import React from "react";
-import styles from "./styles.module.css";
+import Link from 'next/link'
+import React from 'react'
+import styles from './styles.module.css'
 
 export default function ButtonLink({ href, label }) {
   return (
     <div className={styles.link}>
       <Link href={href}>{label}</Link>
     </div>
-  );
+  )
 }
