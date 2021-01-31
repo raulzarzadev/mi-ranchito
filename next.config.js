b907956cdfea7797275d2fe3a9f123b9060339ba
Raul Zarza <raulzarza.dev@gmail.com>
2021-01-31 16:56:20 -0600

    Reconfigurando next module config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
     if (!isServer) {
       config.node = {
         net: 'empty',
+        tls: 'empty'
       }
     }
 
@@ -17,17 +18,8 @@ module.exports = {
     MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
     APP_ID: process.env.APP_ID,
     MEASUREMENT_ID: process.env.MEASUREMENT_ID,
-  },
-  publicRuntimeAdminConfig: {
-    TYPE: process.env.TYPE,
-    PROJECT_ID: process.env.PROJECT_ID,
-    PRIVATE_KEY_ID: process.env.PRIVATE_KEY_ID,
     PRIVATE_KEY: process.env.PRIVATE_KEY,
     CLIENT_EMAIL: process.env.CLIENT_EMAIL,
-    CLIENT_ID: process.env.CLIENT_ID,
-    AUTH_URI: process.env.AUTH_URI,
-    TOKEN_URI: process.env.TOKEN_URI,
-    AUTH_PROVIDER_X509_CERT_URL: process.env.AUTH_PROVIDER_X509_CERT_URL,
-    CLIENT_X509_CERT_URL: process.env.CLIENT_X509_CERT_URL,
   },
+ 
 }
--- secrets.js
+++ secrets.js
@@ -1,27 +1,9 @@
 import getConfig from 'next/config'
-const { publicRuntimeAdminConfig } = getConfig()
-const {
-  TYPE,
-  PROJECT_ID,
-  PRIVATE_KEY_ID,
-  PRIVATE_KEY,
-  CLIENT_EMAIL,
-  CLIENT_ID,
-  AUTH_URI,
-  TOKEN_URI,
-  AUTH_PROVIDER_X509_CERT_URL,
-  CLIENT_X509_CERT_URL,
-} = publicRuntimeAdminConfig
+const { publicRuntimeConfig } = getConfig()
+const { PROJECT_ID, PRIVATE_KEY, CLIENT_EMAIL } = publicRuntimeConfig
 
 export default {
-  type: TYPE,
   project_id: PROJECT_ID,
-  private_key_id: PRIVATE_KEY_ID,
   private_key: PRIVATE_KEY,
   client_email: CLIENT_EMAIL,
-  client_id: CLIENT_ID,
-  auth_uri: AUTH_URI,
-  token_uri: TOKEN_URI,
-  auth_provider_x509_cert_url: AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: CLIENT_X509_CERT_URL,
 }
