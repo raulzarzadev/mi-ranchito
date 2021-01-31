a0c6e637bd5c51ac308b71a366e427f0f3879058
Raul Zarza <raulzarza.dev@gmail.com>
2021-01-31 15:19:09 -0600

    typp

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -4,7 +4,6 @@ import { firebaseConfig } from './firebaseConfig'
 // get this JSON from the Firebase board
 // you can also store the values in environment variables
 import serviceAccount from './secret.json'
-firebaseConfig.databaseURL
 
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
