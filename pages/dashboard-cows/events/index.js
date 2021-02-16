1f42ead781d454086bd5fe8e3c407bad54be20cc
Raul Zarza <raulzarza.dev@gmail.com>
2021-02-16 12:29:43 -0600

    head en events

--- pages/dashboard-cows/events/index.js
+++ pages/dashboard-cows/events/index.js
@@ -1,12 +1,16 @@
 import DashboardCowsLayout from '@cmps/DashboardCowsLayout/DashboardCowsLayout'
 import EventTable from '@cmps/EventTable'
 import useEvents from '@raiz/src/hooks/useEvents'
+import Head from 'next/head'
 import React from 'react'
 
 export default function events() {
   const { events } = useEvents()
   return (
     <>
+      <Head>
+        <title>admin / Eventos</title>
+      </Head>
       <EventTable events={events} title="Todos Los Eventos" />
     </>
   )
--- pages/dashboard-cows/newEvent.js
+++ pages/dashboard-cows/newEvent.js
@@ -1,11 +1,13 @@
 import DashboardCowsLayout from '@cmps/DashboardCowsLayout/DashboardCowsLayout'
 import NewEvent from '@cmps/NewEvent'
-
+import Head from 'next/head'
 
 export default function newEvent() {
-  
   return (
     <>
+      <Head>
+        <title>admin / Nuevo Evento</title>
+      </Head>
       <NewEvent />
     </>
   )
