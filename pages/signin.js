488c8919f6c354dba10be4aaa6772bfca66e880e
Raul Zarza <raulzarza.dev@gmail.com>
2021-02-16 19:24:56 -0600

    fix styles in signin

--- pages/signin.js
+++ pages/signin.js
@@ -2,7 +2,6 @@ import SignForm from '@cmps/SignForm/SignForm'
 import { useAuth } from '@raiz/src/context/AuthContext'
 import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
-import styles from '../styles/sign_page.module.css
 export default function Signin() {
   const router = useRouter()
   const { user } = useAuth()
@@ -10,9 +9,10 @@ export default function Signin() {
     user && router.replace('/')
   }, [user])
   return (
-    <div className={styles.sign_page}>
+    <div >
       <SignForm />
     </div>
   )
 }
 
+  
