471ef5279bc7c1c74f583c0e7ac4c0ad0ba1219a
Raul Zarza <raulzarza.dev@gmail.com>
2021-02-17 20:28:46 -0600

    fix upcoming

--- pages/dashboard-cows/upcoming.js
+++ pages/dashboard-cows/upcoming.js
@@ -1,11 +1,18 @@
+import { Btn1 } from '@cmps/Btns'
 import DashboardCowsLayout from '@cmps/DashboardCowsLayout/DashboardCowsLayout'
 import UpcomingEvents from '@cmps/UpcomingEvents'
+import Head from 'next/head'
 
 export default function upcoming() {
-  
   return (
     <>
-      <UpcomingEvents    />
+      <Head>
+        <title>admin / Proximamente</title>
+      </Head>
+      <div className='box-1'>
+        <Btn1 href="/dashboard-cows/events"> Ver Todos </Btn1>
+      </div>
+      <UpcomingEvents />
     </>
   )
 }
